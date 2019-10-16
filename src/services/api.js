37fcf2b6bc30d6374fb978e886c3d05b5b129901
Samuka <samuelbernardi22@gmail.com>
2019-10-16 11:04:40 -0300

    Correção de rota a API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://app-igj-front.herokuapp.com/'
+    baseURL: 'https://app-igj.herokuapp.com/'
 });
 
 export default api;
